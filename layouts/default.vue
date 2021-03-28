31c9744c90399e0d7b8a3fbfca742bda45f482a5
Samuel Rüegger <samuel@rueegger.me>
2021-03-28 18:29:49 +0200

    iPhone height Bugfix

--- layouts/default.vue
+++ layouts/default.vue
@@ -19,3 +19,12 @@
     }
   }
 </style>
+
+<script>
+export default {
+  mounted() {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }
+}
+</script>
